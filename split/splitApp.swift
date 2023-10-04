0a2cf00d7898e445c018d001551e056e91675574
zawiyarkhan <zawiyarkhan007@gmail.com>
2023-10-04 15:16:36 +0500

    Initial Commit

---
+++ split/splitApp.swift
@@ -0,0 +1,17 @@
+//
+//  splitApp.swift
+//  split
+//
+//  Created by Zawiyar on 04/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct splitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
