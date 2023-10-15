5d9fe803f61c72cca51785bf4f1298cb377d9f0d
Zawiyar Khan <zawiyarkhan007@gmail.com>
2023-10-16 04:02:08 +0500

    UI for Register and login pages

--- split/Views/LoginView.swift
+++ split/Views/LoginView.swift
@@ -7,12 +7,98 @@
 
 import SwiftUI
 
-struct LoginView: View {
+struct CustomTextField: View {
+    @Binding var textController: String
+    var text: String
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack(alignment: .trailing) {
+            RoundedRectangle(cornerRadius: 8)
+                .foregroundColor(Color.white)
+                .shadow(color: Color.black.opacity(1), radius: 0, x: 6, y: 6)
+            
+            TextField(text, text: $textController)
+                .padding(EdgeInsets(top: 8, leading: 16, bottom: 10, trailing: 16))
+                .font(Font.system(size: 22, design: .default))
+                .background(
+                    RoundedRectangle(cornerRadius: 8)
+                        .stroke(Color.black, lineWidth: 3)
+                )
+        }
+    }
+}
+
+struct CustomButton: View {
+    var text:String
+    
+    var body: some View {
+        ZStack(alignment: .trailing) {
+            RoundedRectangle(cornerRadius: 8)
+                .foregroundColor(Color.white)
+                .shadow(color: Color.black.opacity(1), radius: 0, x: 5, y: 5)
+            
+            Text(text)
+                .padding(EdgeInsets(top: 8, leading: 16, bottom: 10, trailing: 16))
+                .font(Font.system(size: 22, design: .default))
+                .background(
+                    RoundedRectangle(cornerRadius: 8)
+                        .stroke(Color.black, lineWidth: 0)
+                )
+        }
+    }
+}
+
+
+struct LoginView: View {
+    @State var username = ""
+    @State var password = ""
+    
+    
+    init() {
+        for family in UIFont.familyNames{
+            print(family)
+            for font in UIFont.fontNames(forFamilyName: family){
+                print(font)
+            }
+        }
+    }
+    
+    var body: some View {
+        VStack{
+            Spacer()
+            Text("SPLIT")
+                .font(.custom("PressStart2P-Regular", size: 50))
+                .shadow(color: .black, radius: 2, x: 2,y:3)
+            Spacer()
+//            Spacer().frame(height: 50)
+            CustomTextField(textController: $username, text: "Username")
+                .frame(width: UIScreen.main.bounds.width/1.15, height: UIScreen.main.bounds.height/17)
+            Spacer().frame(height:20)
+            CustomTextField(textController: $password, text: "Password")
+                .frame(width: UIScreen.main.bounds.width/1.15, height: UIScreen.main.bounds.height/17)
+            Spacer().frame(height:80)
+            CustomButton(text: "Login")
+                .frame(width: 95.0, height: 30)
+                .onTapGesture {
+                    print("Login Button Tapped")
+                }
+            
+            Spacer()
+            
+            Text("Create Your Account")
+                .padding(.all, 30)
+                .bold()
+                .font(.system(size: 20))
+                .shadow(color: .black, radius: 1, x: 1,y: 1)
+                .onTapGesture {
+                    print("Create Button Tapped")
+                }
+        }
     }
 }
 
 #Preview {
     LoginView()
 }
+
+
--- split/Views/RegisterView.swift
+++ split/Views/RegisterView.swift
@@ -8,9 +8,55 @@
 import SwiftUI
 
 struct RegisterView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
+        @State var username = ""
+        @State var password = ""
+        @State var name = ""
+        
+        
+    //    init() {
+    //        for family in UIFont.familyNames{
+    //            print(family)
+    //            for font in UIFont.fontNames(forFamilyName: family){
+    //                print(font)
+    //            }
+    //        }
+    //    }
+        
+        var body: some View {
+            VStack{
+                Spacer()
+                Text("SPLIT")
+                    .font(.custom("PressStart2P-Regular", size: 50))
+                    .shadow(color: .black, radius: 4, x: 2,y:3)
+                Spacer()
+    //            Spacer().frame(height: 50)
+                CustomTextField(textController: $name, text: "Name")
+                    .frame(width: UIScreen.main.bounds.width/1.15, height: UIScreen.main.bounds.height/17)
+                Spacer().frame(height:20)
+                CustomTextField(textController: $username, text: "Username")
+                    .frame(width: UIScreen.main.bounds.width/1.15, height: UIScreen.main.bounds.height/17)
+                Spacer().frame(height:20)
+                CustomTextField(textController: $password, text: "Password")
+                    .frame(width: UIScreen.main.bounds.width/1.15, height: UIScreen.main.bounds.height/17)
+                Spacer().frame(height:80)
+                CustomButton(text: "Register")
+                    .frame(width:UIScreen.main.bounds.width/3.5,height: 30)
+                    .onTapGesture {
+                        print("Login Button Tapped")
+                    }
+                
+                Spacer()
+                
+                Text("Already have an Account")
+                    .padding(.all, 30)
+                    .bold()
+                    .font(.system(size: 20))
+                    .shadow(color: .black, radius: 0.5, x: 1,y: 2)
+                    .onTapGesture {
+                        print("Create Button Tapped")
+                    }
+            }
+        }
 }
 
 #Preview {
