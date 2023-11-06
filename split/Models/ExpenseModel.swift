fdaaca16c619cbb9ff34653095664a4b775946d1
Zawiyar Khan <zawiyarkhan007@gmail.com>
2023-11-07 02:57:00 +0500

    dragGesture + Text Shimmer effect for the swipe

--- split/Models/ExpenseModel.swift
+++ split/Models/ExpenseModel.swift
@@ -6,3 +6,19 @@
 //
 
 import Foundation
+
+//struct Expense: Identifiable, Codable {
+//    var id: UUID
+//    var createdBy: String
+//    var name: String
+//    var friends: Dictionary<String, Any>
+//    // Add more properties as needed
+//
+//    enum CodingKeys: String, CodingKey {
+//        case id
+//        case createdBy
+//        case name
+//        case friends
+//        // Add more cases as needed
+//    }
+//}
--- split/Views/SplitBillView.swift
+++ split/Views/SplitBillView.swift
@@ -9,9 +9,12 @@ import SwiftUI
 
 struct CircularShadowButton: View {
     var icon:String
+    
     var body: some View {
+        
         Circle()
             .frame(width: 45)
+            
             .foregroundColor(.white)
             .shadow(color: .black, radius: 0, x: 6,y: 5)
             .overlay(Circle().stroke(Color.black, lineWidth: 2))
@@ -85,26 +88,60 @@ struct PaymentInfoCard: View {
 
 
 struct SwipeToBar:View {
+    @State var offset: CGSize = .zero
+    @State private var isAnimating = false
+    let screenWidth = UIScreen.main.bounds.width
     var body: some View {
+//        Text("\(offset.width)")
+        let color = Color(red: offset.width < 80 ? 1 : 0, green: offset.width < 80 ? 1 : Double((offset.width + screenWidth/2) / screenWidth), blue: offset.width < 80 ? 1 : 0)
+
+        
         ZStack(alignment: .leading){
             Capsule()
                 .frame(width:UIScreen.main.bounds.width/1.2, height: 50)
                 .background(Capsule().stroke(.black, lineWidth:5))
-                .foregroundColor(.white)
+                .foregroundColor(color)
                 .padding([.horizontal], 10)
                 .shadow(color: .black, radius: 1, x: 5,y: 5)
             
             HStack{
+                
                 CircularShadowButton(icon: "chevron.forward")
                     .padding()
+                    .offset(offset)
+                    .gesture(
+                        DragGesture()
+                            .onChanged{ value in
+                                withAnimation(.smooth()) {
+                                    offset.width = value.translation.width
+                                }
+                            }
+                            .onEnded{ value in
+                                withAnimation(.smooth()){
+                                    offset = .zero                                }
+                                
+                            }
+                    )
+                    
                 
                 Text("Send the bill >>>")
+                    .mask(
+                        LinearGradient(gradient: Gradient(colors: [Color.white.opacity(0.5), Color.white, Color.white.opacity(0.5)]), startPoint: .leading, endPoint: .trailing)
+                            .frame(width: 200, height: 70)
+                            .offset(x: isAnimating ? 200 : -200)
+                    )
+                    .onAppear {
+                        withAnimation(Animation.easeInOut (duration: 2).repeatForever(autoreverses: false)) {
+                            isAnimating = true
+                        }
+                    }
             }
             
         }
         
     }
 }
+
 
 // make a single state variable add all the perecentages to the the same variable
 // the final variable should be checked, if it equals to 100%, we proceed with the flow
