1362250378230297045dbdd8f10409e1fcb57c9d
DuaQadeer <duaqad12@gmail.com>
2023-11-30 20:34:39 +0500

    Added Navigation for HomeView & adjustments to view itself

--- split/Views/HomeView.swift
+++ split/Views/HomeView.swift
@@ -10,32 +10,38 @@ import SwiftUI
 import Firebase
 import FirebaseAuth
 
+import SwiftUI
+import Firebase
+import FirebaseAuth
+
 struct NavigationCard: View {
     var text: String
     var imageIcon: String
+    
+    var view: AnyView
     var body: some View {
-        ZStack(alignment: .center){
-            RoundedRectangle(cornerRadius: /*@START_MENU_TOKEN@*/25.0/*@END_MENU_TOKEN@*/)
-                .foregroundColor(.white)
-                .shadow(color: .black, radius: 1, x: 6,y: 6)
-                .overlay(RoundedRectangle(cornerRadius: /*@START_MENU_TOKEN@*/25.0/*@END_MENU_TOKEN@*/).stroke(.black, lineWidth: 4))
-                .frame(height: UIScreen.main.bounds.height/4.9)
-            HStack(alignment: .center){
-                Spacer()
+        NavigationView{
+            ZStack(alignment: .center){
+                RoundedRectangle(cornerRadius: 15)
+                    .foregroundColor(.white)
+                    .shadow(color: .black, radius: 1, x: 3.5,y: 3.5)
+                    .overlay(RoundedRectangle(cornerRadius: 15).stroke(.black, lineWidth: 2))
+                    .frame(width:UIScreen.main.bounds.width/1.2,height: UIScreen.main.bounds.height/4.9)
+                HStack(alignment: .center){
+                    Spacer()
                     Text(text)
-                        .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                        .fontWeight(.bold)
                         .font(.system(size: 25))
                     Spacer()
                     Image(imageIcon).resizable().scaledToFit().frame(height:100)
                     Spacer()
                 }
-            Spacer()
-            .padding()
-            
-        }.onTapGesture {
-            //ProfileView()
+                Spacer()
+                    .padding()
+                
+            }.onTapGesture {
+            }
         }
-        
     }
 }
 
@@ -44,36 +50,50 @@ struct NavigationCard: View {
 struct HomeView: View {
     
     var body: some View {
-        VStack(alignment: .leading, spacing: 5){
-            
-            Spacer().frame(height:10)
-            
-            HStack{
-                Text("Hello, Username")
-                    .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
-                    .font(.system(size: 25))
-                Spacer()
-                Circle()
-                    .shadow(color:.black, radius: 1,x:4,y:4)
-                    .frame(width:50)
-                    .foregroundColor(.white)
-                    .overlay(
-                        Image(systemName: "person.fill")
-                    )
-                    .overlay(Circle().stroke(Color.black, lineWidth: 2))
+        NavigationView{
+            VStack(alignment: .leading, spacing: 5){
+                
+                Spacer().frame(height:10)
+                
+                HStack{
+                    Text("Hello, Username")
+                        .fontWeight(.bold)
+                        .font(.system(size: 25))
+                    Spacer()
+                    Circle()
+                        .shadow(color:.black, radius: 1,x:4,y:4)
+                        .frame(width:50)
+                        .foregroundColor(.white)
+                        .overlay(
+                            Image(systemName: "person.fill")
+                        )
+                        .overlay(Circle().stroke(Color.black, lineWidth: 2))
+                }
+                
+                Spacer().frame(height: 10)
+                VStack{
+                    NavigationLink(destination: ProfileView(), label: {
+                        NavigationCard(text: "View List of Expenses",imageIcon: "ExpensesList",view: AnyView(ProfileView()))
+
+                    })
+                    .foregroundColor(.black)
+                    NavigationLink(destination: AddExpenseView(), label: {
+                        NavigationCard(text: "AddExpense",imageIcon: "AddNewExpense", view: AnyView(AddExpenseView()))
+                    })
+                    .foregroundColor(.black)
+                    NavigationLink(destination: AddParticipants(), label: {
+                        NavigationCard(text: "Add Friends",imageIcon: "AddFriends", view: AnyView(AddParticipants()))
+                    })
+                    .foregroundColor(.black)
+                    
+                    
+                }
+                
+                
             }
-            
-            Spacer().frame(height: 10)
-            VStack{
-                NavigationCard(text: "View List of Expenses",imageIcon: "ExpensesList")
-                NavigationCard(text: "AddExpense",imageIcon: "AddNewExpense")
-                NavigationCard(text: "Add Friends",imageIcon: "AddFriends")
-             
-            }
-            
+            .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .topLeading)
+            .padding()
         }
-        .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .topLeading)
-        .padding()
     }
 }
 
