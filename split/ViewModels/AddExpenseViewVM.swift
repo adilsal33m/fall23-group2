d021211618c426c84343c72a6f84f8cec36b37c8
DuaQadeer <duaqad12@gmail.com>
2023-11-30 20:39:37 +0500

    Updated AddExpenseView to get the users from firestore

--- split/ViewModels/AddExpenseViewVM.swift
+++ split/ViewModels/AddExpenseViewVM.swift
@@ -6,13 +6,30 @@
 //
 
 import Foundation
+import Firebase
+import FirebaseFirestore
+import Combine
 
 class AddExpenseViewVM: ObservableObject{
     @Published var BillDesc = ""
     @Published var totalString = ""
     @Published var Total = 0.0
     
-    //func ValidateNumber() -> Int?{
-        
-    //}
+    var db = Firestore.firestore()
+    
+    @Published var users:[User] = []
+    
+    func fetchUsers(){
+        db.collection("Users").getDocuments{snapshot, error in
+            if let error = error{
+                print("Error getting documents \(error)")
+            } else{
+                self.users = snapshot?.documents.compactMap{ document in
+                    try? document.data(as: User.self)
+                    
+                } ?? []
+            }
+            
+        }
+    }
 }
