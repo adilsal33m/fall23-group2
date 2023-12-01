30b7e9c2ddd3b316b75b93fc096df6e4b95329fb
Madiha Abid <madiha4naqvi7@gmail.com>
2023-12-01 19:37:30 +0500

    fetching users from firebase + tested

--- split/ViewModels/AddParticipantViewVM.swift
+++ split/ViewModels/AddParticipantViewVM.swift
@@ -6,3 +6,28 @@
 //
 
 import Foundation
+import Firebase
+import FirebaseFirestore
+import Combine
+
+class AddParticipantViewVM: ObservableObject{
+    @Published var search = ""
+    
+    var db = Firestore.firestore()
+    
+    @Published var users:[User] = []
+    
+    func fetchUsers(){
+        db.collection("Users").getDocuments{snapshot, error in
+            if let error = error{
+                print("Error getting documents \(error)")
+            } else{
+                self.users = snapshot?.documents.compactMap{ document in
+                    try? document.data(as: User.self)
+                    
+                } ?? []
+            }
+            
+        }
+    }
+}
--- split/Views/AddParticipants.swift
+++ split/Views/AddParticipants.swift
@@ -7,9 +7,98 @@
 
 import SwiftUI
 
-struct AddParticipants: View {
+struct CustomFriendCard: View {
+    @State private var isSelected = false
+    @State var Name:String
     var body: some View {
-        Text("Add Particpants")
+        ZStack{
+            RoundedRectangle(cornerRadius: 15.0)
+                .frame(height: UIScreen.main.bounds.height/10)
+                .foregroundColor(.white)
+                .shadow(color: .black, radius: 1, x: 3.5, y:3.5)
+                .overlay(RoundedRectangle(cornerRadius: 15.0).stroke(.black, lineWidth: 2))
+            
+            HStack(){
+                
+                CustomPerson(Name: "")
+                Spacer().frame(width: 20)
+                Text("\(Name)")
+                    .padding(.bottom,10)
+                    .bold()
+                
+                Spacer()
+                isSelected ? Image(systemName: "checkmark.circle") : Image(systemName: "circle.dashed")
+            }
+            .padding(.horizontal,20)
+
+        }
+        .onTapGesture {
+            if (isSelected == true){
+                self.isSelected = false
+            }else{
+                self.isSelected = true
+            }
+            
+        }
+        
+    }
+}
+
+struct AddParticipants: View {
+    @StateObject var viewModel = AddParticipantViewVM()
+    var body: some View {
+        VStack(alignment: .leading, content: {
+            HStack{
+                Text("Add Participants")
+                    .bold()
+                    .font(/@START_MENU_TOKEN@/.title/@END_MENU_TOKEN@/)
+                Spacer()
+                Text("Done")
+                    .bold()
+            }
+            ZStack{
+                RoundedRectangle(cornerRadius: 15)
+                    .stroke(style: StrokeStyle(lineWidth: 2, dash: [5]))
+                    .frame(height: UIScreen.main.bounds.height/9)
+                    .padding()
+                HStack(spacing:10){
+                    CustomPerson(Name: "zawiyar")
+                    CustomPerson(Name: "dua")
+                    CustomPerson(Name: "madiha")
+                    
+                }
+            }
+            
+            CustomTextField(textController: $viewModel.search, text: "Search")
+                .frame(width: UIScreen.main.bounds.width/1.15, height: UIScreen.main.bounds.height/17)
+            
+            Text("Friends")
+                .font(.title)
+                .bold()
+                .padding()
+        
+            
+            Spacer().frame(height:30)
+            
+            ScrollView{
+                VStack(spacing: 20){
+                    ForEach(viewModel.users, id: \.id){ user in
+                        CustomFriendCard(Name: user.name)
+                    
+                    }
+                }
+                .padding(.horizontal,10)
+                .onAppear{
+                    viewModel.fetchUsers()
+                }
+            }
+            
+            
+            
+            
+                    
+        })
+        .padding()
     }
 }
 
